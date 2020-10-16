3150de389b5967b30ee9a42dfce8b1c09dcac32b
ajaycheenath@gmail.com <ajaycheenath@gmail.com>
2020-10-16 19:41:45 +0200

    repo url in app db

--- src/main/java/com/cheenath/data/App.java
+++ src/main/java/com/cheenath/data/App.java
@@ -1,59 +1,66 @@
 package com.cheenath.data;
 
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import java.util.Date;
-import java.util.UUID;
 
-@Entity(name="app")
+@Entity(name = "app")
 public class App {
-	@Id
-	private Integer id;
-	private String appName;
-	private int port;
-	private Date createDate;
+    @Id
+    private Integer id;
+    private String appName;
+    private int port;
+    private Date createDate;
+    private String repo;
 
-	protected App(){}
+    protected App() {
+    }
 
-	public App(final Integer id, final String appName, final int port, final Date createDate) {
-		this.id = id;
-		this.appName = appName;
-		this.port = port;
-		this.createDate = createDate;
-	}
+    public App(final Integer id, final String appName, final int port, final Date createDate) {
+        this.id = id;
+        this.appName = appName;
+        this.port = port;
+        this.createDate = createDate;
+    }
 
-	public Integer getId() {
-		return id;
-	}
+    public Integer getId() {
+        return id;
+    }
 
-	public void setId(Integer id) {
-		this.id = id;
-	}
+    public void setId(Integer id) {
+        this.id = id;
+    }
 
-	public String getAppName() {
-		return appName;
-	}
+    public String getAppName() {
+        return appName;
+    }
 
-	public void setAppName(String appName) {
-		this.appName = appName;
-	}
+    public void setAppName(String appName) {
+        this.appName = appName;
+    }
 
-	public int getPort() {
-		return port;
-	}
+    public int getPort() {
+        return port;
+    }
 
-	public void setPort(int port) {
-		this.port = port;
-	}
+    public void setPort(int port) {
+        this.port = port;
+    }
 
-	public Date getCreateDate() {
-		return createDate;
-	}
+    public Date getCreateDate() {
+        return createDate;
+    }
 
-	public void setCreateDate(Date createDate) {
-		this.createDate = createDate;
-	}
+    public void setCreateDate(Date createDate) {
+        this.createDate = createDate;
+    }
+
+    public String getRepo() {
+        return repo;
+    }
+
+    public void setRepo(String repo) {
+        this.repo = repo;
+    }
 
 }
